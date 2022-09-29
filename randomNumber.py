c25ff64d5ac1387298c10612472c57a9a32980b3
melisa.bk <s200204029@ankarabilim.edu.tr>
2022-09-29 19:56:05 +0300

    commit_message

---
+++ randomNumber.py
@@ -0,0 +1,7 @@
+import random
+number=int(input("Aklından bir sayı tut: "))
+if random.randint == number:
+  random.randint(1,100)
+else: 
+  print("sayılar eşleşmedi, lütfen tekrar deneyiniz.")
+  number=int(input("Aklından bir sayı tut: "))
